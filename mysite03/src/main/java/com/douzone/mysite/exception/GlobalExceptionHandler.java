d6d39a73b21de4b837cf5fc201b82e4a45673fa9
yoon-ji-kim <yoonjigim0.0@gmail.com>
2023-02-02 10:51:45 +0900

    logback 적용

--- mysite03/src/main/java/com/douzone/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/com/douzone/mysite/exception/GlobalExceptionHandler.java
@@ -3,26 +3,29 @@ package com.douzone.mysite.exception;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+	private static final Log logger = LogFactory.getLog(GlobalExceptionHandler.class);
 	//어느 exception에 작동하는지
-//	@ExceptionHandler(UserRepositoryException.class)//개별적으로 처리
+	//@ExceptionHandler(UserRepositoryException.class)//개별적으로 처리
 	@ExceptionHandler(Exception.class)
 	public ModelAndView handlerException(Exception e) {
 		//1. 로깅(Logging)
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
-		
-		System.out.println(errors.toString());
+		logger.error(errors.toString());
 		
 		//2. 사과 페이지(3. 정상종료)
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("exception", errors.toString());
 		mav.setViewName("error/exception");
+		
 		//handler가 return하는 것과 같음
 		return mav; //스프링오류
 	}
